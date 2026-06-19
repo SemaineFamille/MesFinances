584814002f3b2f2fa1f26ce1ead34b7c1a568592
SemaineFamille <melanie.weil.81@gmail.com>
2026-06-19 08:33:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+function showScreen(screenId){
 
+  document
+    .querySelectorAll(".screen")
+    .forEach(screen =>
+      screen.classList.remove("active")
+    );
+
+  document
+    .getElementById(screenId)
+    .classList.add("active");
+}
+
+function toggleAssuraForm(){
+
+  const form =
+    document.getElementById("assuraForm");
+
+  form.style.display =
+    form.style.display === "none"
+      ? "block"
+      : "none";
+}
+
+function toggleKptForm(){
+
+  const form =
+    document.getElementById("kptForm");
+
+  form.style.display =
+    form.style.display === "none"
+      ? "block"
+      : "none";
+}
