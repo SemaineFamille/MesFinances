5eb9029b40265e0eba11a7f3d130dfc7537c9ed4
SemaineFamille <melanie.weil.81@gmail.com>
2026-06-21 21:22:26 +0200

    Update google.js

--- google.js
+++ google.js
@@ -1,4 +1,4 @@
-const API_URL ="https://script.google.com/macros/s/AKfycbxcQPcOYKJDse5gU2dAHYk7bzmep0oZ9vEKZZt7b_pdKyDUUGOjWc3_nJPFr3Fk6JOWIg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxcQPcOYKJDse5gU2dAHYk7bzmep0oZ9vEKZZt7b_pdKyDUUGOjWc3_nJPFr3Fk6JOWIg/exec";
 
 /* =========================
    APPEL API
